5945daa8954114ac8dfd73b332c42e4f0f8772d5
flavienbwk <flavien@berwick.fr>
2021-01-22 02:18:01 +0100

    Added UTF8 support and num_sentences or ratio options

--- summarize.py
+++ summarize.py
@@ -6,6 +6,8 @@ def run():
     parser = argparse.ArgumentParser(description='Process and summarize lectures')
     parser.add_argument('-path', dest='path', default=None, help='File path of lecture')
     parser.add_argument('-model', dest='model', default='bert-large-uncased', help='')
+    parser.add_argument('-num-sentences', dest='num_sentences', default=-1, help='Will return X sentences')
+    parser.add_argument('-ratio', dest='ratio', default=-1, help='Will return a ratio of sentences from the text length (0.2 is a good value)')
     parser.add_argument('-hidden', dest='hidden', default=-2, help='Which hidden layer to use from Bert')
     parser.add_argument('-reduce-option', dest='reduce_option', default='mean', help='How to reduce the hidden layer from bert')
     parser.add_argument('-greedyness', dest='greedyness', help='Greedyness of the NeuralCoref model', default=0.45)
@@ -22,8 +24,14 @@ def run():
         hidden=args.hidden,
         reduce_option=args.reduce_option
     )
-
-    print(model(text_data))
+    
+    if int(args.num_sentences) >= 0:
+        result = model(text_data, num_sentences=int(args.num_sentences))
+    elif float(args.ratio) >= 0.0:
+        result = model(text_data, ratio=float(args.num_sentences))
+    else:
+        result = model(text_data)
+    print(result)
 
 
 if __name__ == '__main__':
